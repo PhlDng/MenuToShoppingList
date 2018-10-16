27b1ba6c97875a6be58809918007ef73f8519e5d
PhlDng <35849997+PhlDng@users.noreply.github.com>
2018-10-16 11:21:30 +0200

    Basic structure of data exporter & able to send messages with telegram

--- data-exporter.py
+++ data-exporter.py
@@ -1,1 +1,28 @@
 #This is where all the functions related to sending out the data willl be stored
+
+#import packages
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+#import keys
+import keys
+
+
+
+def send_to_telegram(list_to_send = ["Eggs", "Milk"]):
+
+    # creating a bot that will send messages
+    TelegramBot = telegram.Bot(keys.key_telegram_bot)
+
+    for element in list_to_send:
+    # sending a message for every element in the list
+        TelegramBot.send_message(chat_id=keys.telegram_chat_ids[0], #currently only send messages to me
+                                 text=element,
+                                 parse_mode="Markdown")
+
+def send_to_wunderlist(list_to_send=["Eggs", "Milk"]):
+    print ("ToDo: Wunderlist")
+
+def send_to_email(list_to_send=["Eggs", "Milk"])
+    print ("ToDo: E-Mail")
+
---
+++ keys.py
@@ -0,0 +1,4 @@
+#This is where all the keys for API, etc will be stored - should not be uploaded to GitHub
+
+key_telegram_bot = "697615040:AAGqdfL8_Bvm-vf4mXHy64wf3_Qfpmj5Y_M"
+telegram_chat_ids = [435398227] #Id Philipp
