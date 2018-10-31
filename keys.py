daa61bac362b437134270ecad3139b56c7787aed
PhlDng <35849997+PhlDng@users.noreply.github.com>
2018-10-31 12:44:21 +0100

    Added blank keys.py file

---
+++ keys.py
@@ -0,0 +1,14 @@
+#This is where all the keys for API, etc will be stored - should not be uploaded to GitHub
+
+#Data for Telegram
+key_telegram_bot = #Telegram bot Token as string
+telegram_admin_ids = [] #Telegram Chat IDs as int
+
+#Data for Emails
+email_user = #as string
+email_password = #as string
+
+#Data for Wunderlist
+wunderlist_client_id = #as string
+wunderlist_client_secret = #as string
+wunderlist_access_token = #as string
