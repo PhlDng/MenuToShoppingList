bf40dfd641f92aafa7746115dbc0d039bb0962c3
PhlDng <35849997+PhlDng@users.noreply.github.com>
2018-11-02 14:22:50 +0100

    Update and rename keys.py to config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#This is where all the keys for API, etc will be stored - should not be uploaded to GitHub
+
+#Data for Telegram
+key_telegram_bot = "" #string
+telegram_admin_ids = [] #list of numbers
+
+#Data for Emails
+email_user = "" #string
+email_password = "" #string
+email_smtp_adr = '' #string
+email_smtp_port = #int
