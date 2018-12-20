6ea580b4adebfbd2e1aefe925a26e14b66f51858
PhlDng <35849997+PhlDng@users.noreply.github.com>
2018-12-20 15:41:08 +0100

    Deleted info in config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #This is where all the keys for API, etc will be stored - should not be uploaded to GitHub
 
 #Data for Telegram
-key_telegram_bot = "697615040:AAGqdfL8_Bvm-vf4mXHy64wf3_Qfpmj5Y_M" #string
+key_telegram_bot = "" #string
 
 #Data for Emails
-email_user = "MenuToGroceryList@mail.ch" #string
-email_password = "2e2307dc777dd72a4aec01fb383bca9a" #string
-email_smtp_adr = 'smtp.mail.ch' #string
-email_smtp_port = 587
+email_user = "" #string
+email_password = "" #string
+email_smtp_adr = '' #string
+email_smtp_port = #integer
